02f5915aafd05843ec1b8900d0543f7db7e7f4f4
Kacper Fila <kacperfila26@gmail.com>
2023-03-04 18:35:08 +0100

    Project restored
    -start add MediatR

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,13 +1,18 @@
+using System.Reflection;
 using Application.Abstractions;
 using DataAccess;
 using DataAccess.Repositories;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DevelopConnectionString");
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddDbContext<FitFoodBookDbContext>(opt => opt.UseSqlServer(connectionString));
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
+app.MapGet("hello", (IMediator mediator) => "hello");
+
 
 app.Run();
